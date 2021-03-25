542b7116d2351d5a747ce33e00b9a521430a6c88
Antonio Rangel Chaves <57496213+AntonioRangelC@users.noreply.github.com>
2021-03-24 23:42:07 -0300

    Inserção de rotas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Wrapper from './pages/Wrapper';
+import CadastroMedico from './pages/CadastroMedicoPage'
 import {GlobalStyles} from './styles/Global'
+import AlreadyLoggedRoute from './utils/Routes/AlreadyLoggedRoute';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import ProtectedRoute from './utils/Routes/ProtectedRoute'
+
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Wrapper />
+          {/* <Wrapper /> */}
+          <Switch>
+            <ProtectedRoute path="/cadastro/medico" render={() => <CadastroMedico />} />
+            <Route path="/login" render={() => <LoginPage />} />
+            <AlreadyLoggedRoute path="/homepage" render={() => <HomePage />}/>
+          </Switch>
       </BrowserRouter>
       <GlobalStyles />
     </>
