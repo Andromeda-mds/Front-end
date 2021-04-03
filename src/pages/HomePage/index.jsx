f3c597b2df403f0abc4d579d53806679f6c922ed
Antonio Rangel Chaves <57496213+AntonioRangelC@users.noreply.github.com>
2021-04-03 09:24:18 -0300

    Criando botões da homepage de secretário

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -2,6 +2,13 @@ import React from "react";
 
 import * as home from "./style";
 import Header from "../../Components/Header";
+import FindInPageIcon from '@material-ui/icons/FindInPage';
+import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
+import CalendarToday from "@material-ui/icons/CalendarToday";
+import AddBoxIcon from '@material-ui/icons/AddBox';
+import AddIcon from '@material-ui/icons/Add';
+import AccountBoxIcon from '@material-ui/icons/AccountBox';
+import SearchIcon from '@material-ui/icons/Search';
 
 const HomePage = () => {
 
@@ -24,6 +31,28 @@ const HomePage = () => {
                 <h1>Sou secretário</h1>
               </div>
             }
+          </div>
+          <div className="buttonSection">
+            <home.BuscarMedicosButton>
+              <div className="buscarMedicos">
+                <AccountBoxIcon style={{  fontSize: 70 }} />
+                <SearchIcon style={{  fontSize: 60 }}/>
+              </div>
+              <h1>Buscar Médicos</h1>
+            </home.BuscarMedicosButton>
+            <home.BuscarPacientesButton>
+              <FindInPageIcon style={{  fontSize: 70 }}/>
+              <h1>Buscar ficha de paciente</h1>
+            </home.BuscarPacientesButton>
+            <home.NovaConsultaButton>
+              <div className="calendario">
+                <CalendarToday style={{  fontSize: 70 }}/>
+                <div className="add">
+                  <AddIcon style={{  fontSize: 30}}/>
+                </div>
+              </div>
+              <h1>Nova consulta</h1>
+            </home.NovaConsultaButton>
           </div>
         </home.ContentPage>
       </home.Container>
--- src/pages/HomePage/style.js
+++ src/pages/HomePage/style.js
@@ -22,5 +22,107 @@ export const ContentPage = styled.div`
   .content-div{
       display: flex;
       flex-direction: column;
+      width: 100%;
+      height: 15%;
   }
+  .buttonSection{
+      display: flex;
+      flex-direction: row;
+      width: 100%;
+      height: 40%;
+      padding: 4rem;
+  }
+  
+`;
+
+export const BuscarMedicosButton = styled.button`
+    width: 12.5rem;
+    height: 11rem;
+    border-radius: 1.5rem;
+    box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.25);
+    background-color: ${Theme.colors.$sispocPrimary};
+    color: #1B1B1B;
+    border: none;
+    transition: all 0.3s ease 0s;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-left: 15%;
+    &:hover {
+        background-color: #ff7800;
+        box-shadow: 0px 15px 20px rgba(46, 229, 157, 0.4);
+        color: #fff;
+        transform: translateY(-7px);
+    }
+    >h1{
+        font-weight: 600;
+        width: 80%;
+    }
+    .buscarMedicos{
+      display: flex;
+      flex-direction: row;
+    }
+
+`;
+
+export const BuscarPacientesButton = styled.button`
+    width: 12.5rem;
+    height: 11rem;
+    border-radius: 1.5rem;
+    box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.25);
+    background-color: ${Theme.colors.$sispocPrimary};
+    color: #1B1B1B;
+    border: none;
+    transition: all 0.3s ease 0s;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-left: 15%;
+    &:hover {
+        background-color: #ff7800;
+        box-shadow: 0px 15px 20px rgba(46, 229, 157, 0.4);
+        color: #fff;
+        transform: translateY(-7px);
+    }
+    >h1{
+        font-weight: 600;
+        width: 80%;
+    }
+    
+
+`;
+
+export const NovaConsultaButton = styled.button`
+    width: 12.5rem;
+    height: 11rem;
+    border-radius: 1.5rem;
+    box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.25);
+    background-color: ${Theme.colors.$sispocPrimary};
+    color: #1B1B1B;
+    border: none;
+    transition: all 0.3s ease 0s;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-left: 15%;
+    &:hover {
+        background-color: #ff7800;
+        box-shadow: 0px 15px 20px rgba(46, 229, 157, 0.4);
+        color: #fff;
+        transform: translateY(-7px);
+    }
+    >h1{
+        font-weight: 600;
+        width: 80%;
+    }
+    .calendario{
+      display: flex; 
+      justify-content: center;
+      margin-bottom: 1rem;
+    }
+    
+
 `;
