3b7494db8d090e8dd7aad818ffff72acac3fbfe0
Antonio Rangel Chaves <57496213+AntonioRangelC@users.noreply.github.com>
2021-04-07 11:48:56 -0300

    Adicionando a visualização do User role

--- src/pages/CadastroMedicoPage/styles.js
+++ src/pages/CadastroMedicoPage/styles.js
@@ -8,6 +8,11 @@ export const Container = styled.div`
     display: flex;
     flex-direction: column;
     /* align-items: center; */
+    .content{
+        height:89%;
+        width:100%;
+        
+    }
 `;
 
 export const Barra = styled.div`
@@ -27,17 +32,39 @@ export const formDiv = styled.div`
     height: 90%;
     width: 100%;
     display: flex;
-    justify-content: center;
-
+    flex-direction: column;
+    //background-color: green;
 `; 
 
 export const Titulo = styled.div`
     width: 100%;
     height: 15%;
     padding: 2rem;
+    display: flex;
     >h1{
         font-size: 2rem;
         line-height: 40px;
     }
 `;
+
+export const HeaderDiv = styled.div`
+  height: 11%;
+`;
+
+export const PersonDiv = styled.div`
+  width: 10%;
+  height: 32%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  margin-left: 65%;
+  >span{
+    cursor: pointer;
+    :hover{
+      color: lightgrey;
+      text-decoration: underline;
+    }
+  }
+`; 
 
