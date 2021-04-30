49a18cc86f5b050d035ab37a12099e2c9e3d676a
Antonio Rangel Chaves <57496213+AntonioRangelC@users.noreply.github.com>
2021-04-30 17:29:05 -0300

    Fazendo requisições do back-end

--- src/pages/AgendaMedico/index.jsx
+++ src/pages/AgendaMedico/index.jsx
@@ -10,10 +10,41 @@ import {
   Agenda,
   Inject,
   DragAndDrop,
-  Resize
+  Resize,
 } from "@syncfusion/ej2-react-schedule";
+import decode from "jwt-decode";
+import axios from "axios";
+import { backendURL } from "../../services/api"
 
 const AgendaMedico = () => {
+  const [token, setToken] = React.useState(localStorage.getItem("loginToken"));
+  const [role, setRole] = React.useState(localStorage.getItem("role"));
+  //const [userId, setUserId] = React.useState("");
+  const [agendaMedico, setAgendaMedico] = React.useState([]);
+  var userId;
+
+  const handleClientId = async () => {
+    userId = await decode(localStorage.getItem("loginToken"));
+  }
+
+  const fetchMedicoData = () => {
+    axios.get(`${backendURL}agenda/medico/${userId._id}`, 
+            {headers: {"x-access-token": `${token}`}}
+            ).then(res => {
+              console.log(res);
+              setAgendaMedico(res.item.horariosDisponiveis);
+              console.log("Agenda: ", agendaMedico);
+            }).catch(err => {
+              console.log(err);
+            })
+  }
+
+  React.useEffect(async () => {
+    await handleClientId();
+    console.log(userId);
+    fetchMedicoData();
+  },[]);
+
   return (
     <S.Wrapper>
       <S.HeaderDiv>
@@ -21,8 +52,18 @@ const AgendaMedico = () => {
       </S.HeaderDiv>
       <S.Content>
         <S.AgendaDiv>
-          <ScheduleComponent>
-            <Inject services={[Day, Week, WorkWeek, Month, Agenda, DragAndDrop, Resize]} />
+          <ScheduleComponent readonly={role === "medico" ? true : false}>
+            <Inject
+              services={[
+                Day,
+                Week,
+                WorkWeek,
+                Month,
+                Agenda,
+                DragAndDrop,
+                Resize,
+              ]}
+            />
           </ScheduleComponent>
         </S.AgendaDiv>
       </S.Content>
