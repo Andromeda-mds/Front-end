2c44d6defdbd5c24d0520665985c0460ce24d032
AntonioRangelC <antonio.rangel.02@gmail.com>
2023-10-07 18:10:47 -0300

    teste3
    Co-authored-by: Vitor Diniz Pagani Vieira Ribeiro <vitordiniz25@gmail.com>

--- src/pages/AgendaMedico/index.jsx
+++ src/pages/AgendaMedico/index.jsx
@@ -35,6 +35,7 @@ const AgendaMedico = (props) => {
   const location = useLocation();
   //fffff
   //gggg
+  // lllll
   const handleClientId = async () => {
     userId = await decode(localStorage.getItem("loginToken"));
   };
